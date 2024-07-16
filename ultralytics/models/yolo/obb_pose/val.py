77078f64a716277127d1ba496214a4e402736dca
morning tea <51917814+yzqxy@users.noreply.github.com>
2024-07-16 19:07:44 +0800

    Update val.py

--- ultralytics/models/yolo/obb_pose/val.py
+++ ultralytics/models/yolo/obb_pose/val.py
@@ -109,7 +109,7 @@ class OBB_PoseValidator(DetectionValidator):
         """Prepares and scales keypoints in a batch for pose processing."""
         predn = super()._prepare_pred(pred, pbatch)
         nk = pbatch["kpts"].shape[1]
-        pred_kpts = predn[:, 6:].view(len(predn), nk, -1)
+        pred_kpts = predn[:, 7:].view(len(predn), nk, -1)
         ops.scale_coords(pbatch["imgsz"], pred_kpts, pbatch["ori_shape"], ratio_pad=pbatch["ratio_pad"])
         return predn, pred_kpts
 
